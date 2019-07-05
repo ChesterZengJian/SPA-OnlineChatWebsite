028014c95be9b657176319633663c19caca6b392
JianBen <374488168@qq.com>
2019-07-06 01:13:28 +0800

    1. add the people model  function

--- spa/js/spa.model.js
+++ spa/js/spa.model.js
@@ -13,6 +13,7 @@
 /**global $, spa */
 
 spa.model = (function () {
+    'use strict';
     // ---------- BEGIN MODULE SCOPE VARIABLES --------------
     // Begin the people object API
     // The people object is available at spa.model.people
@@ -44,8 +45,68 @@ spa.model = (function () {
     //  * name - the string name of the user.
     //  * css_map - a map of attributes used for avatar presentation.
     // 
-    
     // End the people object API
+
+    var
+        configMap = {
+            anon_id: 'a0'
+        }
+        , stateMap = {
+            anon_user: null
+            , people_cid_map: {}
+            , people_db: TAFFY()
+        }
+        , isFakeData = true
+        , personProto
+        , makePerson
+        , people
+        , initModule
+        ;
+
+    personProto = {
+        get_is_user: function () {
+            return this.cid === stateMap.user.cid;
+        }
+        , get_is_anon: function () {
+            return this.cid === stateMap.anon_user.cid;
+        }
+    }
+
+    makePerson = function (person_map) {
+        var
+            person
+            , cid = person_map.cid
+            , css_map = person_map.css_map
+            , id = person_map.id
+            , name = person_map.name;
+
+        if (cid === undefined || !name) {
+            throw 'client id and name required';
+        }
+
+        person = Object.create(personProto);
+        person.cid = cid;
+        person.name = name;
+        person.css_map = css_map;
+
+        if (id) {
+            person.id = id;
+        }
+
+        stateMap.people_cid_map[cid] = person;
+
+        stateMap.people_db.insert(person);
+        return person;
+    }
+
+    people = {
+        get_db: function () {
+            return stateMap.people_db;
+        }
+        , get_cid_map: function () {
+            return stateMap.people_cid_map;
+        }
+    }
     // ---------- END MODULE SCOPE VARIABLES ----------------
 
     // ---------- BEGIN UTILITY METHODS ---------------------
@@ -58,6 +119,35 @@ spa.model = (function () {
     // ---------- END EVENT HANDLERS ------------------------
 
     // ---------- BEGIN PULIC METHODS -----------------------
-    return {};
+    initModule = function () {
+        var
+            i
+            , people_list
+            , person_map;
+
+        stateMap.anon_user = makePerson({
+            cid: configMap.anon_id
+            , id: configMap.anon_id
+            , name: 'anonymous'
+        });
+        stateMap.user = stateMap.anon_user;
+
+        if (isFakeData) {
+            people_list = spa.fake.getPeopleList();
+            for (i = 0; i < people_list.length; i++) {
+                person_map = people_list[i];
+                makePerson({
+                    cid: person_map._id
+                    , css_map: person_map.css_map
+                    , id: person_map._id
+                    , name: person_map.name
+                });
+            }
+        }
+    };
+    return {
+        initModule: initModule
+        , people: people
+    };
     // ---------- END PULIC METHODS -------------------------
 }());
