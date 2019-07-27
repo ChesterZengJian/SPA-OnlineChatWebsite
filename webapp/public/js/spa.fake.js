3c833d53415328a3626c3bef676289abd3781346
JianBen <374488168@qq.com>
2019-07-28 01:10:56 +0800

    1. complete the chat model 2. optimize the chat module UI

--- webapp/public/js/spa.fake.js
+++ webapp/public/js/spa.fake.js
@@ -18,6 +18,7 @@ spa.fake = (function () {
         getPeopleList
         , fakeIdSerial
         , makeFakeId
+        , peopleList
         , mockSio;
     fakeIdSerial = 5;
     // ---------- END MODULE SCOPE VARIABLES ----------------
@@ -75,29 +76,144 @@ spa.fake = (function () {
                 }
             }
         ]
-    }
+    };
+
+    peopleList = [
+        {
+            name: 'Betty'
+            , _id: 'id_01'
+            , css_map: {
+                top: 20
+                , left: 20
+                , 'background-color': 'rbg(128,255,128)'
+            }
+        }
+        , {
+            name: 'Mike'
+            , _id: 'id_02'
+            , css_map: {
+                top: 60
+                , left: 20
+                , 'background-color': 'rbg(128,255,128)'
+            }
+        }
+        , {
+            name: 'Pebbles'
+            , _id: 'id_03'
+            , css_map: {
+                top: 100
+                , left: 20
+                , 'background-color': 'rbg(128,255,128)'
+            }
+        }
+        , {
+            name: 'Pebbles'
+            , _id: 'id_04'
+            , css_map: {
+                top: 100
+                , left: 20
+                , 'background-color': 'rbg(128,255,128)'
+            }
+        }
+    ];
 
     mockSio = (function () {
         var
             on_sio
             , emit_sio
+            , send_listchange
+            , listchange_idto
+            , emit_mock_msg
             , callback_map = {};
-        on_sio = function (msg_type,callback) {
-            callback_map[msg_type]=callback;
+        on_sio = function (msg_type, callback) {
+            callback_map[msg_type] = callback;
         };
         emit_sio = function (msg_type, data) {
+            var person_map
+                , i;
             if (msg_type === 'addUser' && callback_map.userupdate) {
                 setTimeout(function () {
-                    callback_map.userupdate(
-                        [{
-                            _id: makeFakeId()
-                            , name: data.name
-                            , css_map: data.css_map
-                        }]
-                    );
+                    person_map = {
+                        _id: makeFakeId()
+                        , name: data.name
+                        , css_map: data.css_map
+                    };
+                    peopleList.push(person_map);
+                    callback_map.userupdate([person_map]);
+                    // callback_map.userupdate(
+                    //     [{
+                    //         _id: makeFakeId()
+                    //         , name: data.name
+                    //         , css_map: data.css_map
+                    //     }]
+                    // );
                 }, 3000)
             }
+
+            if (msg_type === 'updatechat' && callback_map.updatechat) {
+                setTimeout(() => {
+                    var user = spa.model.people.get_user();
+                    callback_map.updatechat([{
+                        dest_id: user._id
+                        , dest_name: user.name
+                        , sender_id: data.dest_id
+                        , msg_text: 'Thank you ' + user.name
+                    }]);
+                }, 2000);
+            }
+
+            if (msg_type === 'leavechat') {
+                delete callback_map.listchange;
+                delete callback_map.updatechat;
+
+                if (listchange_idto) {
+                    clearTimeout(listchange_idto);
+                    listchange_idto = undefined;
+                }
+                send_listchange();
+            }
+
+            if (msg_type === 'updateavatar' && callback_map.listchange) {
+                for (i = 0; i < peopleList.length; i++) {
+                    if (peopleList[i]._id === data.person_id) {
+                        peopleList[i].css_map = data.css_map;
+                        break;
+                    }
+                }
+                callback_map.listchange([peopleList]);
+            }
         };
+
+        emit_mock_msg = function () {
+            setTimeout(() => {
+                var user = spa.model.people.get_user();
+                if (callback_map.updatechat) {
+                    callback_map.updatechat([{
+                        dest_id: user.id
+                        , dest_name: user.name
+                        , sender_id: 'id_04'
+                        , msg_text: 'Hi, there is ' + user.name + ' .'
+                    }])
+                }
+                else {
+                    emit_mock_msg();
+                }
+            }, 8000);
+        }
+
+        send_listchange = function () {
+            listchange_idto = setTimeout(() => {
+                if (callback_map.listchange) {
+                    callback_map.listchange([peopleList]);
+                    emit_mock_msg();
+                    listchange_idto = undefined;
+                } else {
+                    send_listchange();
+                }
+            }, (1000));
+        };
+        send_listchange();
+
         return {
             emit: emit_sio
             , on: on_sio
@@ -105,8 +221,9 @@ spa.fake = (function () {
     }());
 
     return {
-        getPeopleList: getPeopleList
-        , mockSio: mockSio
+        mockSio: mockSio
+        // ,peopleList
+        , getPeopleList: getPeopleList
     };
     // ---------- END PULIC METHODS -------------------------
 }());
--- webapp/public/js/spa.model.js
+++ webapp/public/js/spa.model.js
@@ -15,37 +15,7 @@
 spa.model = (function () {
     'use strict';
     // ---------- BEGIN MODULE SCOPE VARIABLES --------------
-    // Begin the people object API
-    // The people object is available at spa.model.people
-    // The people object provides methods and events to manage
-    // a collection of person objects. Its public methods include:
-    //  * get_user()
-    //  * get_db()
-    //  * get_by_cid(<client_id>)
-    //  * login(<user_name>)
-    //  * logout()
-    // 
-    // jQuery global custom events published by the objecct include:
-    //  * 'spa-login' is published when a user login process completes. 
-    // The updated user object is provided as data
-    //  * 'spa-logout' is published when a logout completes.
-    // The format user object is provided as data
-    // 
-    // Each person is represented by a person object.
-    // Person objects provide the following methods:
-    //  * get_is_user() - return true if object is the current user
-    //  * get_is_anon() - return true if object is annoymous
-    // 
-    // The attributes for a person object include:
-    //  * cid - string client id. This is always defined, 
-    // and is only different from the id attribute if the client
-    // data is not synced with the backend.
-    //  * id - the unique id. This may be undefined if the object is 
-    // not synced with the backend.
-    //  * name - the string name of the user.
-    //  * css_map - a map of attributes used for avatar presentation.
-    // 
-    // End the people object API
+
 
     var
         configMap = {
@@ -129,6 +99,7 @@ spa.model = (function () {
         stateMap.user.id = user_map._id;
         stateMap.user.css_map = user_map.css_map;
         stateMap.people_cid_map[user_map._id] = stateMap.user;
+        chat.join();
 
         $.gevent.publish('spa-login', [stateMap.user]);
     }
@@ -148,6 +119,37 @@ spa.model = (function () {
         return true;
     }
 
+    // Begin the people object API
+    // The people object is available at spa.model.people
+    // The people object provides methods and events to manage
+    // a collection of person objects. Its public methods include:
+    //  * get_user()
+    //  * get_db()
+    //  * get_by_cid(<client_id>)
+    //  * login(<user_name>)
+    //  * logout()
+    // 
+    // jQuery global custom events published by the objecct include:
+    //  * 'spa-login' is published when a user login process completes. 
+    // The updated user object is provided as data
+    //  * 'spa-logout' is published when a logout completes.
+    // The format user object is provided as data
+    // 
+    // Each person is represented by a person object.
+    // Person objects provide the following methods:
+    //  * get_is_user() - return true if object is the current user
+    //  * get_is_anon() - return true if object is annoymous
+    // 
+    // The attributes for a person object include:
+    //  * cid - string client id. This is always defined, 
+    // and is only different from the id attribute if the client
+    // data is not synced with the backend.
+    //  * id - the unique id. This may be undefined if the object is 
+    // not synced with the backend.
+    //  * name - the string name of the user.
+    //  * css_map - a map of attributes used for avatar presentation.
+    // 
+    // End the people object API
     people = (function () {
         var
             get_by_cid
@@ -191,11 +193,13 @@ spa.model = (function () {
 
         logout = function () {
             var
-                is_removed
-                , user = stateMap.user;
+                // is_removed
+                user = stateMap.user;
 
+            chat._leave();
             is_removed = removePerson(user);
             stateMap.user = stateMap.anon_user;
+            clearPeopleDb();
 
             $.gevent.publish('spa-logout', [user]);
             return is_removed;
@@ -232,15 +236,23 @@ spa.model = (function () {
     chat = (function () {
         var
             _publish_listchange
+            , _publish_updatechat
+            , get_chatee
+            , send_msg
+            , set_chatee
             , _update_list
             , _leave_chat
-            , join_chat;
+            , join_chat
+            , chatee = null
+            , update_avatar;
 
         _update_list = function (arg_list) {
             var
                 i
+                , person
                 , person_map
                 , make_person_map
+                , is_chatee_online = false
                 , people_list = arg_list[0];
 
             clearPeopleDb();
@@ -259,6 +271,10 @@ spa.model = (function () {
                     continue PERSON;
                 }
 
+                if (chatee && chatee.id === make_person_map.id) {
+                    is_chatee_online = true;
+                }
+
                 make_person_map = {
                     cid: person_map._id
                     , css_map: person_map.css_map
@@ -266,10 +282,32 @@ spa.model = (function () {
                     , name: person_map.name
                 };
 
-                makePerson(make_person_map);
+                person = makePerson(make_person_map);
+
+                if (chatee && chatee.id === make_person_map.id) {
+                    is_chatee_online = true;
+                    chatee.person;
+                }
             }
             stateMap.people_db.sort('name');
+
+            if (chatee && !is_chatee_online) {
+                set_chatee('');
+            }
         };
+
+        _publish_updatechat = function (arg_list) {
+            var msg_map = arg_list[0];
+
+            if (!chatee) {
+                set_chatee(msg_map.sender_id);
+            }
+            else if (msg_map.sender_id !== stateMap.user.id && msg_map.sender_id !== chatee.id) {
+                set_chatee(msg_map.sender_id);
+            }
+
+            $.gevent.publish('spa-updatechat', [msg_map]);
+        }
 
         _publish_listchange = function (arg_list) {
             _update_list(arg_list);
@@ -278,11 +316,16 @@ spa.model = (function () {
 
         _leave_chat = function () {
             var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            chatee = null;
             stateMap.is_connected = false;
             if (sio) {
                 sio.emit('leavechat');
             }
         };
+
+        get_chatee = function () {
+            return chatee;
+        }
 
         join_chat = function () {
             var sio;
@@ -298,13 +341,69 @@ spa.model = (function () {
 
             sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
             sio.on('listchange', _publish_listchange);
+            sio.on('updatechat', _publish_updatechat);
             stateMap.is_connected = true;
             return true;
         };
 
+        send_msg = function (msg_text) {
+            var msg_map
+                , sio = isFakeData ? spa.fake.mockSio : spa.getSio();
+
+            if (!sio) {
+                return false;
+            }
+
+            if (!(stateMap.user && chatee)) {
+                return false;
+            }
+
+            msg_map = {
+                dest_id: chatee.id
+                , dest_name: chatee.name
+                , sender_id: stateMap.user.id
+                , msg_text: msg_text
+            };
+
+            _publish_updatechat([msg_map]);
+            sio.emit('updatechat', msg_map);
+            return true;
+        };
+
+        set_chatee = function (person_id) {
+            var new_chatee;
+            new_chatee = stateMap.people_cid_map[person_id];
+            if (new_chatee) {
+                if (chatee && chatee.id === new_chatee.id) {
+                    return false;
+                }
+            }
+            else {
+                new_chatee = null;
+            }
+
+            $.gevent.publish('spa-setchatee', {
+                old_chatee: chatee
+                , new_chatee: new_chatee
+            });
+            chatee = new_chatee;
+            return true;
+        };
+
+        update_avatar = function (avatar_update_map) {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            if (sio) {
+                sio.emit('updateavatar', avatar_update_map);
+            }
+        };
+
         return {
             _leave: _leave_chat
+            , get_chatee: get_chatee
+            , send_msg: send_msg
+            , set_chatee: set_chatee
             , join: join_chat
+            , update_avatar: update_avatar
         };
     }());
     // ---------- END UTILITY METHODS -----------------------
